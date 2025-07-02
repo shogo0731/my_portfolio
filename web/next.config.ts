af9e5c60cee5f76eeef194c012cd152b23fb2f41
suisan0731 <suisan0731@gmail.com>
2025-07-03 08:10:36 +0900

    環境構築

---
+++ web/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+
+  /* 左下のNマークを削除*/
+  devIndicators: false,
+};
+
+export default nextConfig;
