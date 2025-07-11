69446eecc60748036063ce36814f7d220263a712
suisan0731 <suisan0731@gmail.com>
2025-07-11 22:52:43 +0900

    プロジェクトを増やした

--- app/src/app/const.tsx
+++ app/src/app/const.tsx
@@ -44,13 +44,38 @@ export const skills = [
 ];
 
 export const projects = [
+    {
+    title: "my-portfolio",
+    description: "このポートフォリオサイト自体です。",
+    tech: ["TypeScript", "React", "Next.js", "Git", "Docker", "Vercel"],
+    image: "/image/my-portfolio.png",
+    liveDemo: "https://my-portfolio-nzmp.vercel.app/",
+    github: "https://github.com/shogo0731/my_portfolio"
+  },
   {
     title: "mentaAI",
     description:
       "AIを活用したメンタルヘルス支援アプリ。睡眠時間や体調を管理し、それらの情報を基にAIモデルと会話できる機能がある。",
-    tech: ["Dart", "Flutter", "Go", "Firebase", "GCP"],
-    image: "/project/mentaAI.png",
+    tech: ["Dart", "Flutter", "Go", "Firebase", "GCP", "Git", "Docker"],
+    image: "/image/mentaAI.png",
     liveDemo: "https://health-care-app-3e333.web.app/",
     github: "https://github.com/health-Care-App/health_care_app",
+  },
+  {
+    title: "Beatlink",
+    description:
+      "音楽とSNSを組み合わせたプラットフォーム。ユーザーは音楽を共有し、他のユーザーと交流できる。",
+    tech: ["TypeScript", "React", "Next.js", "Tailwind CSS","Git", "Docker", "Vercel", "Convex"],
+    image: "/image/Beatlink.png",
+    liveDemo: "https://deployed-beetlink.vercel.app",
+    github: "https://github.com/UtakataKyosui/DeployedBeatlink"
+  },
+  {
+    title: "コエレシピ",
+    description:"音声対話でレシピのガイドをしてくれるWebアプリケーション。音声でレシピ手順を案内してくれる。",
+    tech: ["TypeScript", "React", "Next.js", "Tailwind CSS","Git", "Docker", "Vercel"],
+    image: "/image/coe-recipe.png",
+    liveDemo: "https://hands-free-cooking.vercel.app/home",
+    github: "https://github.com/2024-vol11-18/hands-free-cooking"
   },
 ];
